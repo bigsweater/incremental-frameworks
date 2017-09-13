d91112b9323c496a355f62c5769bfb6662997333
Vincent Maglione <vincent@bluebench.co>
2017-09-13 12:20:20 -0400

    Modular CSS

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,33 +3,31 @@
 @section('body')
 <section class="jumbotron text-center">
 	<div class="container">
-		<h1 class="jumbotron-heading">Album example</h1>
-		<p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+		<h1 class="jumbotron-heading">Hero headline</h1>
+		<p class="lead text-muted">Here is some hero content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quos omnis magni corrupti amet quas pariatur odio tenetur voluptatum atque, veniam ea accusantium id, ratione unde eos velit officiis expedita.</p>
 		<p>
-			<a href="#" class="btn btn-primary">Main call to action</a>
-			<a href="#" class="btn btn-secondary">Secondary action</a>
+			<a href="#" class="btn btn-primary">Primary Call to Action</a>
+			<a href="#" class="btn btn-secondary">Secondary Call to Action</a>
 		</p>
 	</div>
 </section>
 
 <div class="album text-muted">
 	<div class="container">
-
 		<div class="row">
 			<div class="card">
-				<img alt="100%x280" style="height: 280px; width: 100%; display: block;" src="https://unsplash.it/g/356/280?random&1">
+				<img alt="100%x280" style="height: 280px; width: 100%; display: block;" src="https://placehold.it/356x280">
 				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 			</div>
 			<div class="card">
-				<img alt="100%x280" src="https://unsplash.it/g/356/280?random&2" style="height: 280px; width: 100%; display: block;">
+				<img alt="100%x280" src="https://placehold.it/356x280" style="height: 280px; width: 100%; display: block;">
 				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 			</div>
 			<div class="card">
-				<img alt="100%x280" src="https://unsplash.it/g/356/280?random&3" style="height: 280px; width: 100%; display: block;">
+				<img alt="100%x280" src="https://placehold.it/356x280" style="height: 280px; width: 100%; display: block;">
 				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 			</div>
 		</div>
-
 	</div>
 </div>
 @endsection
